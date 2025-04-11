b6a49ee8a7262fe8576c97d1ba82ea721384ed53
Maxim Gorin <onlinexmax@gmail.com>
2025-04-11 19:15:59 +0300

    Add enum for retest

--- src/patterns/retest.rs
+++ src/patterns/retest.rs
@@ -2,39 +2,53 @@ use crate::candle::Candle;
 
 const LEVEL_TOLERANCE_PERCENT: f64 = 2.0;
 const NEAR_RETEST_PERIOD: usize = 10;
+const LONG_RETEST_PERIOD: usize = 50;
 
-pub fn is_near_retest(candles: &[impl Candle], level: f64) -> bool {
+#[derive(Debug, Clone, Copy, PartialEq, Hash, Ord, PartialOrd, Eq)]
+pub enum RetestType {
+    Near,
+    Long,
+}
+
+pub fn is_retest(candles: &[impl Candle], level: f64) -> Option<RetestType> {
     if candles.len() < 3 {
-        return false;
+        return None;
     }
 
     //let mut candles = candles.to_vec();
     //candles.sort_by(|a, b| a.get_time_key().cmp(&b.get_time_key()));
 
     let mut bumps_count = 0;
+    let mut result = None;
 
     for (index, candle) in candles.iter().enumerate() {
         let bump_dir = bumped_into_level(candle, level, LEVEL_TOLERANCE_PERCENT);
-        
+
         if bump_dir.is_some() {
             bumps_count += 1;
         }
 
         if index == 0 && bump_dir.is_none() {
-            return false;
+            return None;
         }
 
         if index == candles.len() - 2 && bump_dir.is_some() {
             // candle before last candle also bumped into level so we are near level
-            return false;
+            return None;
         }
 
-        if index > NEAR_RETEST_PERIOD {
-            return false;
+        if index <= NEAR_RETEST_PERIOD && bumps_count >= 2 {
+            result = Some(RetestType::Near);
+            break;
+        }
+
+        if index <= LONG_RETEST_PERIOD && bumps_count >= 2 {
+            result = Some(RetestType::Near);
+            break;
         }
     }
 
-    bumps_count >= 2
+    result
 }
 
 fn bumped_into_level(
@@ -103,8 +117,8 @@ mod tests {
             },
         ];
 
-        let result = super::is_near_retest(&candles, 7.0);
+        let result = super::is_retest(&candles, 7.0);
 
-        assert!(!result);
+        assert_eq!(result, None);
     }
 }
