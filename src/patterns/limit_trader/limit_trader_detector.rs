029bf4fd2cbb823f014394d46a2d96b1b43da5a9
yuriy-smplt <pluriw@gmail.com>
2025-05-04 10:14:57 +0300

    updated

--- src/patterns/limit_trader/limit_trader_detector.rs
+++ src/patterns/limit_trader/limit_trader_detector.rs
@@ -8,12 +8,13 @@ const LTD_MIN_DEPTH: usize = 3;
 #[derive(Debug, Clone)]
 pub struct LimitTraderDetectorPattern {
     pub tolerance_percent: f64,
-    pub min_depth: usize,
+    pub window_size: usize,
 }
 
 #[derive(Debug, Clone)]
 pub struct LimitTraderSignal {
-    pub level: f64,               
+    pub level: f64,  
+    pub date_time_key: u64,             
     pub side: LimitTraderSide, 
 }
 
@@ -27,7 +28,7 @@ impl Default for LimitTraderDetectorPattern {
     fn default() -> Self {
         Self {
             tolerance_percent: LTD_TOLERANCE_PERCENT,
-            min_depth: LTD_MIN_DEPTH,
+            window_size: LTD_MIN_DEPTH,
         }
     }
 }
@@ -36,14 +37,17 @@ impl LimitTraderDetectorPattern {
     pub fn detect<T: Candle>(&self, candles: &BTreeMap<u64, T>) -> Option<LimitTraderSignal> {
         let candle_vec: Vec<&T> = candles.values().rev().collect();
 
-        if candle_vec.len() < self.min_depth {
+        if candle_vec.len() < self.window_size {
             return None;
         }
 
-        for window in candle_vec.windows(self.min_depth) {
+        for window in candle_vec.windows(self.window_size) {
             // average high and low
             let avg_high = window.iter().map(|c| c.get_high()).sum::<f64>() / window.len() as f64;
             let avg_low = window.iter().map(|c| c.get_low()).sum::<f64>() / window.len() as f64;
+            let date_time= window.first().unwrap().get_time_key();
+            let high= window.first().unwrap().get_high();
+            let low= window.first().unwrap().get_low();
 
             // check highs near avg_high
             let highs_near = window.iter().all(|c| {
@@ -63,12 +67,14 @@ impl LimitTraderDetectorPattern {
 
             if highs_near && up_count > 0 && down_count > 0 {
                 return Some(LimitTraderSignal {
-                    level: avg_high,
+                    level: high,
+                    date_time_key: date_time,
                     side: LimitTraderSide::Seller,
                 });
             } else if lows_near && up_count > 0 && down_count > 0 {
                 return Some(LimitTraderSignal {
-                    level: avg_low,
+                    level: low,
+                    date_time_key: date_time,
                     side: LimitTraderSide::Buyer,
                 });
             }
@@ -86,9 +92,11 @@ mod tests {
     #[test]
     fn detects_limit_seller() {
         let candles = vec![
-            CandleInstance { time_key: 1, open: 100.0, close: 99.0, high: 105.0, low: 98.0, volume: 1.0, },
-            CandleInstance { time_key: 2, open: 99.0, close: 100.5, high: 104.8, low: 97.5, volume: 1.0, },
-            CandleInstance { time_key: 3, open: 101.0, close: 100.0, high: 105.1, low: 99.0, volume: 1.0, },
+            CandleInstance { time_key: 1, open: 100.0, close: 99.0, high: 105.0, low: 98.0, volume: 1.0, },  // down
+            CandleInstance { time_key: 2, open: 99.0, close: 100.5, high: 104.8, low: 97.5, volume: 1.0, },  // up   +
+            CandleInstance { time_key: 3, open: 101.0, close: 100.0, high: 105.1, low: 99.0, volume: 1.0, }, // down +
+            CandleInstance { time_key: 4, open: 101.0, close: 100.0, high: 105.05, low: 99.0, volume: 1.0, },// down +
+            CandleInstance { time_key: 5, open: 101.0, close: 100.0, high: 105.75, low: 99.0, volume: 1.0, },// down
         ];
 
         let mut map = BTreeMap::new();
@@ -102,15 +110,18 @@ mod tests {
         assert!(result.is_some());
         let signal = result.unwrap();
         assert_eq!(signal.side, LimitTraderSide::Seller);
+        assert_eq!(signal.level, 105.05);
         println!("Detected Seller at {:.2}", signal.level);
     }
 
     #[test]
     fn detects_limit_buyer() {
         let candles = vec![
-            CandleInstance { time_key: 1, open: 95.0, close: 96.5, high: 96.5, low: 90.0, volume: 1.0, },
-            CandleInstance { time_key: 2, open: 96.0, close: 95.0, high: 95.0, low: 90.2, volume: 1.0, },
-            CandleInstance { time_key: 3, open: 94.5, close: 96.0, high: 96.8, low: 90.1, volume: 1.0, },
+            CandleInstance { time_key: 1, open: 95.0, close: 96.5, high: 96.5, low: 90.0, volume: 1.0, },  // up
+            CandleInstance { time_key: 2, open: 96.0, close: 95.0, high: 95.0, low: 90.2, volume: 1.0, },  // down +
+            CandleInstance { time_key: 3, open: 94.5, close: 96.0, high: 96.8, low: 90.1, volume: 1.0, },  // up   +
+            CandleInstance { time_key: 4, open: 94.5, close: 96.0, high: 96.8, low: 90.05, volume: 1.0, }, // up   +
+            CandleInstance { time_key: 5, open: 94.5, close: 96.0, high: 96.8, low: 90.75, volume: 1.0, }, // up
         ];
 
         let mut map = BTreeMap::new();
@@ -124,6 +135,7 @@ mod tests {
         assert!(result.is_some());
         let signal = result.unwrap();
         assert_eq!(signal.side, LimitTraderSide::Buyer);
+        assert_eq!(signal.level, 90.05);
         println!("Detected Buyer at {:.2}", signal.level);
     }
 
